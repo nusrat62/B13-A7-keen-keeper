0e04b7ce0e43e05dc8d94001717fc4150d72fcce
nusrat62 <nj6111449@gmail.com>
2026-04-16 15:06:26 +0600

    chore: configure project setup for production

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         
         <h1 className="text-lg font-semibold text-gray-800">
-          KeenKeeper
+          KeenKe
         </h1>
 
 
